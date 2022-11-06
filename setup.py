d2b84e87da14df44920cca70cb78e4fed6fa2774
Michael Fuerst <mail@michaelfuers.de>
2022-11-06 22:26:46 +0100

    Bumped version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '20201010'
+__version__ = '20221106'
 
 here = path.abspath(path.dirname(__file__))
 
